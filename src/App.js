36fce760f0e3265b9726972395b001cff391df91
lesterroan <lester23roan@gmail.com>
2021-01-22 23:31:56 +0800

    added 3rd input to concatenation; aligned video input horizontally

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   const [vid2Start, setVid2Start] = useState(0);
   const [vid3Start, setVid3Start] = useState(0);
 
-  const [gif, setGif] = useState();
+  const [finalVid, setFinalVid] = useState();
 
   const load = async () => {
     await ffmpeg.load();
@@ -54,8 +54,8 @@ function App() {
 
 
 
-    await ffmpeg.run("-i", 'vid1.mp4', "-i", 'vid2.mp4',
-      "-filter_complex", "[0:v:0]trim=start=00:00:00:end=00:00:03,setdar=16/9,scale=1920x1080,setpts=PTS-STARTPTS[v0],[0:a:0]atrim=start=00:00:00:end=00:00:10,asetpts=PTS-STARTPTS[a0],[1:v:0]trim=start=00:00:00:end=00:00:03,setdar=16/9,scale=1920x1080,setpts=PTS-STARTPTS[v1],[1:a:0]atrim=start=00:00:00:end=00:00:03,asetpts=PTS-STARTPTS[a1],[v0][a0][v1][a1]concat=n=2:v=1:a=1[outv][outa]", "-vsync", "2", "-map", "[outv]", "-map", "[outa]", '-s', '1920x1080', "output.mp4")
+    await ffmpeg.run("-i", 'vid1.mp4', "-i", 'vid2.mp4', "-i", 'vid3.mp4',
+      "-filter_complex", "[0:v:0]trim=start=00:00:00:end=00:00:03,setdar=16/9,scale=1920x1080,setpts=PTS-STARTPTS[v0],[0:a:0]atrim=start=00:00:00:end=00:00:03,asetpts=PTS-STARTPTS[a0],[1:v:0]trim=start=00:00:00:end=00:00:03,setdar=16/9,scale=1920x1080,setpts=PTS-STARTPTS[v1],[1:a:0]atrim=start=00:00:00:end=00:00:03,asetpts=PTS-STARTPTS[a1],[2:v:0]trim=start=00:00:00:end=00:00:03,setdar=16/9,scale=1920x1080,setpts=PTS-STARTPTS[v2],[2:a:0]atrim=start=00:00:00:end=00:00:03,asetpts=PTS-STARTPTS[a2],[v0][a0][v1][a1][v2][a2]concat=n=3:v=1:a=1[outv][outa]", "-vsync", "2", "-map", "[outv]", "-map", "[outa]", '-s', '1920x1080', "output.mp4")
 
     //read result 
     const data = ffmpeg.FS('readFile', 'output.mp4');
@@ -63,7 +63,7 @@ function App() {
     //create a url to view it in the browser, blob just means raw file in thiss case binary 
     // pass the MIME type???
     const url = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
-    setGif(url);
+    setFinalVid(url);
   }
 
   const loadVideo = file => new Promise((resolve, reject) => {
@@ -99,7 +99,7 @@ function App() {
       const min = time.substr(3, 2)
       const sec = time.substr(6, 2)
 
-      setVid1Start({ "dfdf": 524154545 })
+      setVid1Start({ h: hour, m: min, s: sec })
 
     } else if (vidNum === "vid2") {
       console.log("vid2");
@@ -119,42 +119,63 @@ function App() {
   return ready ? (
     <div className="App">
 
-      { video1 && <video
-        controls
-        width="320" height="240"
-        src={URL.createObjectURL(video1)}>
-      </video>}
+      <h1>Cut and Merge Progressive Web Applications</h1>
 
-      <TimeFormat videoLength={vid1Start} />
-      <hr />
+      <div className="centerDiv">
+        <div className="videoGroup" >
+          {video1 && <video
+            controls
+            width="240" height="160"
+            src={URL.createObjectURL(video1)}>
+          </video>}
+          {!video1 && <canvas id="myCanvas" width="240" height="160" style={{ background: "black" }}>
+          </canvas>}
+          <TimeFormat videoLength={vid1Start} />
+          <input type="file" accept="video/*" onChange={(inputVideo) => handleVideoInput(inputVideo, "vid1")} />
+        </div>
 
-      { video2 && <video
-        controls
-        width="320" height="240"
-        src={URL.createObjectURL(video2)}>
-      </video>}
 
-      <hr />
-      { video3 && <video
-        controls
-        width="320" height="240"
-        src={URL.createObjectURL(video3)}>
-      </video>}
+        <div className="videoGroup">
+          {video2 && <video
+            controls
+            width="240" height="160"
+            src={URL.createObjectURL(video2)}>
+          </video>}
+          {!video2 && <canvas id="myCanvas" width="240" height="160" style={{ background: "black" }}>
+          </canvas>}
+          <input type="file" accept="video/*" onChange={(e) => setVideo2(e.target.files?.item(0))} />
+        </div>
 
-      <input type="file" accept="video/*" onChange={(inputVideo) => handleVideoInput(inputVideo, "vid1")} />
-      <input type="file" accept="video/*" onChange={(inputVideo) => handleVideoInput(inputVideo, "vid2")} />
-      <input type="file" accept="video/*" onChange={(e) => setVideo3(e.target.files?.item(0))} />
 
-      <h3>Resule</h3>
+
+
+        <div className="videoGroup">
+          {video3 && <video
+            controls
+            width="240" height="160"
+            src={URL.createObjectURL(video3)}>
+          </video>}
+
+          {!video3 && <canvas id="myCanvas" width="240" height="160" className="emptyVideo">
+          </canvas>}
+
+          <input type="file" accept="video/*" onChange={(e) => setVideo3(e.target.files?.item(0))} />
+        </div>
+
+      </div>
+
+      <h3>Result</h3>
 
       <button onClick={convertToGif}>Convert</button>
 
-      {gif && <video
-        controls
-        width="20vw"
-        src={gif}></video>}
+      {
+        finalVid && <video
+          controls
+          width="320" height="240"
+          src={finalVid}></video>
+      }
 
-    </div>
+    </div >
   ) : (<p>Loading FFmpeg</p>)
 }
 
@@ -165,7 +186,14 @@ const TimeFormat = ({ videoLength }) => {
 
 
   return (<>
-    <DurationDropDown durationInput={videoLength.h} /> : <DurationDropDown durationInput={videoLength.m} /> : <DurationDropDown durationInput={videoLength.s} />
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "transparent"
+    }}>
+      <DurationDropDown durationInput={videoLength.h} /> : <DurationDropDown durationInput={videoLength.m} /> : <DurationDropDown durationInput={videoLength.s} />
+    </div>
   </>)
 }
 
